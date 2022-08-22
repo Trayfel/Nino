54e71dd9b217564f7fe3f6d93a5decc037846254
Ashley Louis <59287645+Trayfel@users.noreply.github.com>
2022-08-22 01:21:27 +0100

    Add files via upload

---
+++ commands/info/botinfo.js
@@ -0,0 +1,46 @@
+const Command = require('../Command.js');
+const { MessageEmbed } = require('discord.js');
+const pkg = require(__basedir + '/package.json');
+const { owner } = require('../../utils/emojis.json');
+const { oneLine, stripIndent } = require('common-tags');
+
+module.exports = class BotInfoCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'botinfo',
+      aliases: ['bot', 'bi'],
+      usage: 'botinfo',
+      description: 'Fetches Nino\'s bot information.',
+      type: client.types.INFO
+    });
+  }
+  run(message) {
+    const botOwner = message.client.users.cache.get(message.client.ownerId);
+    const prefix = message.client.db.settings.selectPrefix.pluck().get(message.guild.id);
+    const tech = stripIndent`
+      Version     :: ${pkg.version}
+      Library     :: Discord.js v12.3.1
+      Environment :: Node.js v12.16.3
+      Database    :: SQLite
+    `;
+    const embed = new MessageEmbed()
+      .setTitle('Nino\'s Bot Information')
+      .setDescription(oneLine`
+        Nino is a new Discord bot that aims to be an all in one bot for your every need.
+        She first went live on **August 21st 2022**.
+      `)
+      .addField('Prefix', `\`${prefix}\``, true)
+      .addField('Client ID', `\`${message.client.user.id}\``, true)
+      .addField(`Developer ${owner}`, botOwner, true)
+      .addField('Tech', `\`\`\`asciidoc\n${tech}\`\`\``)
+      .addField(
+        'Links', 
+        '**[Invite Me](https://discord.com/api/oauth2/authorize?client_id=1009002446913286266&permissions=8&scope=bot) | ' +
+        '[Support Server](https://discord.gg/uKYmSDG9yS)**'
+      )
+      .setFooter(message.member.displayName,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(message.guild.me.displayHexColor);
+    message.channel.send(embed);
+  }
+};
